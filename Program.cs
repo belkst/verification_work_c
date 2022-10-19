863e312429b6f75ac6ae2b9f8a44a26f7d80b87d
Aleksandr Novikov <belkst@mail.ru>
2022-10-19 23:00:11 +0300

    Add method set size array

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//The Verification work
+
+Console.Clear();
+
+int SetSizeArray() // set size array
+{
+    Console.WriteLine("Задайте размер массива, с значением больше 5 элементов");
+    int size = Convert.ToInt32(Console.ReadLine());
+    while (size<=4)
+    {
+        Console.WriteLine("Вы ввели неверное значение (4 или меньше), пожалуйста, повторите ввод:");
+        size = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+    }
+    return size;
+}
+
+
+
+SetSizeArray();

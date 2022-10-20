ed36a9b344178240c02c50a7ff5331ba806a1c3a
Aleksandr Novikov <belkst@mail.ru>
2022-10-20 19:11:49 +0300

    Готовлю мердж

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ int SetSizeArray() // set size array
 {
     Console.WriteLine("Задайте размер массива, с значением больше 5 элементов");
     int size = Convert.ToInt32(Console.ReadLine());
-    while (size<=4)
+    while (size <= 4)
     {
         Console.WriteLine("Вы ввели неверное значение (4 или меньше), пожалуйста, повторите ввод:");
         size = Convert.ToInt32(Console.ReadLine());
@@ -15,6 +15,16 @@ int SetSizeArray() // set size array
     return size;
 }
 
+string[] NewFillArrayString(int sizeArray)
+{
+    string[] newFillArray = new string[sizeArray];
+    for (int i = 0; i < sizeArray; i++)
+    {
+        newFillArray[i] = new Random().Next(0, 1000).ToString();
+        System.Console.WriteLine(newFillArray[i]);
+    }
+    return newFillArray;
+}
 
 
-SetSizeArray();
+NewFillArrayString(SetSizeArray());

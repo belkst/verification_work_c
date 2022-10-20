d45580cd44d3bbf6019ec5c2a95ab0fb6bc33250
Aleksandr Novikov <113017435+belkst@users.noreply.github.com>
2022-10-20 16:06:10 +0300

    Create new mixed strings array

--- Program.cs
+++ Program.cs
@@ -2,19 +2,30 @@
 
 Console.Clear();
 
-int SetSizeArray() // set size array
+// int SetSizeArray() // set size array
+// {
+//     Console.WriteLine("Задайте размер массива, с значением больше 5 элементов");
+//     int size = Convert.ToInt32(Console.ReadLine());
+//     while (size<=4)
+//     {
+//         Console.WriteLine("Вы ввели неверное значение (4 или меньше), пожалуйста, повторите ввод:");
+//         size = Convert.ToInt32(Console.ReadLine());
+//         Console.WriteLine();
+//     }
+//     return size;
+// }
+
+string[] mixedArray = {"Але3к2с32андр23 Пу465шкин на7ча7л пи7с7а7ть св7ои п45ервые прои87зв87едения уже в 876с45емь лет4."}
+
+
+string[] ParceNumber(string mixedArray)
 {
-    Console.WriteLine("Задайте размер массива, с значением больше 5 элементов");
-    int size = Convert.ToInt32(Console.ReadLine());
-    while (size<=4)
+string[] newStrArray = string[];
+for(i=0; i<mixedArray.Length; i++)
     {
-        Console.WriteLine("Вы ввели неверное значение (4 или меньше), пожалуйста, повторите ввод:");
-        size = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine();
+    
     }
-    return size;
+    
+return newStrArray;
 }
-
-
-
 SetSizeArray();

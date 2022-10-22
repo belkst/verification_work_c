3e59f224d612c1626e3efac29942f5cada1a062f
Aleksandr Novikov <belkst@mail.ru>
2022-10-22 12:09:43 +0300

    Допилил код программы

--- Program.cs
+++ Program.cs
@@ -1,35 +1,35 @@
 //The Verification work
 
-Console.Clear();
+Console.Clear(); //очищаем консоль
 
 
-Console.Write("Enter length array:");
-int sizeArray = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter length array:"); 
+int sizeArray = Convert.ToInt32(Console.ReadLine()); //получаем размер массива строк
 
 string[] mixedArray = new string[sizeArray];
 
-for (int i = 0; i < sizeArray; i++)
+for (int i = 0; i < sizeArray; i++)//заполняем массив строк
 {
     Console.Write($"Enter {i + 1} element: ");
     mixedArray[i] = Console.ReadLine();
 }
 
-string newElement = "";
-string[] finalArray = mixedArray;
+string newElement = "";//создали строковую переменную, для записи в нее посимвольной "очистки"
+string[] finalArray = mixedArray;//результирующий массив
 int j = 0;
 
-foreach (string item in mixedArray)
+foreach (string item in mixedArray)//перебираем массив строк
 {
-    for (int i = 0; i < item.Length; i++)
+    for (int i = 0; i < item.Length; i++)//перебираем строку посимвольно
     {
-        if (item[i] < '0' || item[i] > '9')
+        if (item[i] < '0' || item[i] > '9')//делаем выборку
         {
-            newElement += item[i];
+            newElement += item[i];//записываем выборку в строковую переменную
         }
     }
-    finalArray[j] = newElement;
+    finalArray[j] = newElement;//записываем полученную строку в элемент массива
     j++;
-    newElement = "";
+    newElement = "";//обнуляем строковую переменную
 }
-Console.WriteLine(String.Join("-", finalArray));
+Console.WriteLine(String.Join("-", finalArray));//печатаем результирующий массив
 Console.WriteLine();
